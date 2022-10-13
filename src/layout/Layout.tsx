c2e98de11f032ef1499fd4ef459874b66b011f3d
Camilo Castrillon <jcastrillon@be-prosolutions.com>
2022-10-12 19:27:16 -0500

    feat: instaling sass and folder strcture modified

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Navbar from '../components/NavBar'
+
+export default function Layout() {
+  return (
+    <div>
+        Este es mi Layout
+        <Header></Header>
+        <Navbar></Navbar>
+        <Footer></Footer>
+    </div>
+  )
+}
