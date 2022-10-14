92864f90d2d175fba1b66fdf320de2038d151af2
Camilo Castrillon <jcastrillon@be-prosolutions.com>
2022-10-14 00:00:49 -0500

    chore(card): card body second version

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import CardBody from './CardBody'
 import CardImage from '../../assets/cherry.png'
 import {ReactComponent as StarIcon}  from '../../assets/icons/ic_star.svg'
@@ -6,23 +6,39 @@ import {ReactComponent as HeartIcon}  from '../../assets/icons/ic-favorite.svg'
 
 
 export default function Card() {
+  const [isShown, setIsShown] = useState(false);
+
+
   return (
-    <div className='card_container'>
-        <img className='card_image' src={CardImage} alt="card_image" />
-        <span className='card_title'>
+    <div 
+      className={isShown ? 'card_container open' : 'card_container'}
+      onMouseEnter={() => setIsShown(true)}
+      onMouseLeave={() => setIsShown(false)}
+    >
+      <img className='card_image' src={CardImage} alt="card_image" />
+      
+      {!isShown && (
+        <span 
+          className='card_title'
+        >
             <div className="card_food">Ojingeo</div>
             <div className="card_ingredient">Muchim</div>
         </span>
-        <CardBody/>
-        <div className='card_icons'>
-            <span className='card_puntuation'>
-                <StarIcon/>
-                <span className='card_puntuation_text'>5.0</span>
-            </span>
-            <span className='card_favorite'>
-              <HeartIcon/>
-            </span>
+      )}
+      <CardBody/>
+      {!isShown && (
+        <div 
+          className='card_icons'
+        >
+          <span className='card_puntuation'>
+              <StarIcon/>
+              <span className='card_puntuation_text'>5.0</span>
+          </span>
+          <span className='card_favorite'>
+            <HeartIcon/>
+          </span>
         </div>
+      )}
     </div>
   )
 }
