0db8527e3763ca94aabd8b07324a649fbff788e1
Camilo Castrillon <jcastrillon@be-prosolutions.com>
2022-10-16 13:25:06 -0500

    feat(store) redux toolkit added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Layout from "./layout/Layout";
+import { store } from './store';
+import { Provider } from 'react-redux'
+import { Counter } from './components/Counter';
 
 function App() {
   return (
-    <Layout/>
+    <Provider store={store}>
+      {/* <Layout/> */}
+      <Counter/>
+    </Provider>
   );
 }
 
